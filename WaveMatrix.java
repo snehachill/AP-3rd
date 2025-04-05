d36333ef093e9753bfef68c1eda3ba32f789e34a
snehachill <st957553@gmail.com>
2025-04-05 10:48:59 +0530

    Update WaveMatrix.java

--- WaveMatrix.java
+++ WaveMatrix.java
@@ -1,20 +1,18 @@
 public class WaveMatrix {
     public static void printWave(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-            // If the row index is even, traverse left to right
             if (i % 2 == 0) {
                 for (int j = 0; j < matrix[i].length; j++) {
                     System.out.print(matrix[i][j] + " ");
                 }
             } 
-            // If the row index is odd, traverse right to left
-            else {
+             else {
                 for (int j = matrix[i].length - 1; j >= 0; j--) {
                     System.out.print(matrix[i][j] + " ");
                 }
             }
         }
-        // Print a newline for better output formatting
+        
         System.out.println();
     }
 
